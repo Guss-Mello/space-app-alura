fe641ced986fe07317b886e42f2717033bc1e9b5
Gustavo Mello Soares dos Santos <gustavo@digicom.com.br>
2025-08-19 18:45:32 -0300

    Arrumei alguns bugs, modifiquei o codigo e fiz aparecer as imagens da galeria

--- space-app/src/App.jsx
+++ space-app/src/App.jsx
@@ -1,28 +1,30 @@
-import { styled } from "styled-components";
-import EstilosGlobais from "./assets/componentes/EstilosGlobais";
-import Cabecalho from "./assets/componentes/Cabecalho";
-import BarraLateral from "./assets/componentes/BarraLateral";
-import Banner from "./assets/componentes/Banner";
-import bannerBackground from "./assets/banner.png";
-import Galeria from "./assets/componentes/Galeria";
+import { styled } from "styled-components"
+import EstilosGlobais from "./assets/componentes/EstilosGlobais"
+import Cabecalho from "./assets/componentes/Cabecalho"
+import BarraLateral from "./assets/componentes/BarraLateral"
+import Banner from "./assets/componentes/Banner"
+import bannerBackground from './assets/banner.png'
+import Galeria from "./assets/componentes/Galeria"
+
+import fotos from './fotos.json'
+import { useState } from "react"
 
 const FundoGradiente = styled.div`
   background: linear-gradient(174.61deg, #041833 4.16%, #04244F 48%, #154580 96.76%);
   width: 100%;
   min-height: 100vh;
-  
-`;
+`
 
 const AppContainer = styled.div`
   width: 1440px;
   margin: 0 auto;
   max-width: 100%;
-`;
+`
 
 const MainContainer = styled.main`
   display: flex;
   gap: 24px;
-`;
+`
 
 const ConteudoGaleria = styled.section`
   display: flex;
@@ -30,26 +32,23 @@ const ConteudoGaleria = styled.section`
   flex-grow: 1;
 `
 
-function App() {
-
-
+const App = () => {
+  const [fotosDaGaleria, setFotosDaGaleria] = useState(fotos)
   return (
     <FundoGradiente>
       <EstilosGlobais />
       <AppContainer>
-
         <Cabecalho />
         <MainContainer>
-
           <BarraLateral />
-
           <ConteudoGaleria>
-            <Banner texto="A galeria mais completa de fotos do espaco!" backgroundImage={bannerBackground} />
-            <Galeria />
+            <Banner
+              texto="A galeria mais completa de fotos do espaço!"
+              backgroundImage={bannerBackground}
+            />
+            <Galeria fotos={fotosDaGaleria}/>
           </ConteudoGaleria>
-
         </MainContainer>
-
       </AppContainer>
     </FundoGradiente>
   )
